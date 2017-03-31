681d61e36e6eec6f0dff193aeedf6bb4205f2ccd
De Narm <s3152806@msx.tu-dresden.de>
2017-03-31 02:44:03 +0200

    backlight fix

--- copy.sh
+++ copy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cp ~/.vimrc ~/.config/i3/config ~/.xinitrc ~/Files/Configs
+cp ~/.vimrc ~/.config/i3/config ~/.xinitrc ~/Files/Configs 
+cp ~/../../etc/X11/xorg.conf ~/Files/Configs
 pacman -Qqe > pacman.txt
