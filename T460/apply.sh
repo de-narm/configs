23b53d7cc6c0ddc5a6a0361e27642b706754e3cd
De Narm <s3152806@msx.tu-dresden.de>
2023-06-24 00:16:24 +0200

    update 06/2023

---
+++ T460/apply.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp ~/Files/Configs/T460/.vimrc ~
+cp ~/Files/Configs/T460/clojure.vim ~/.vim/syntax/clojure.vim
+cp ~/Files/Configs/T460/i3/config ~/.config/i3
+cp ~/Files/Configs/T460/i3status/config ~/.config/i3status
+cp ~/Files/Configs/T460/.xinitrc ~
+cp ~/Files/Configs/T460/xorg.conf ~/../../etc/X11/
+cp ~/Files/Configs/T460/.ycm_extra_conf.py ~
+cp ~/Files/Configs/T460/.tmux.conf ~
+cp ~/Files/Configs/T460/compton.conf ~/.config
+cp ~/Files/Configs/T460/COMMANDS.td ~
