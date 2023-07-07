5a96edc2c106837bf8426ca5092be9d8b207aed6
De Narm <sebastian.b.c.dn@gmail.com>
2023-07-07 16:06:35 +0200

    P53 new script

--- P53/apply.sh
+++ P53/apply.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cp ~/Files/Configs/P53/.vimrc ~
-cp ~/Files/Configs/P53/clojure.vim ~/.vim/syntax/clojure.vim
-cp ~/Files/Configs/P53/i3/config ~/.config/i3/
-cp ~/Files/Configs/P53/i3status/config ~/.config/i3status/
-cp ~/Files/Configs/P53/.xinitrc ~
-cp ~/Files/Configs/P53/90-backlight.conf /etc/udev/rules.d/
-cp ~/Files/Configs/P53/.ycm_extra_conf.py ~
-cp ~/Files/Configs/P53/.tmux.conf ~
-cp ~/Files/Configs/P53/compton.conf ~/.config
-cp -r ~/Files/Configs/P53/Scripts ~/Files/Scripts
+cp ~/Files/Conf/P53/.vimrc ~
+cp ~/Files/Conf/P53/clojure.vim ~/.vim/syntax/clojure.vim
+cp ~/Files/Conf/P53/i3/config ~/.config/i3/
+cp ~/Files/Conf/P53/i3status/config ~/.config/i3status/
+cp ~/Files/Conf/P53/.xinitrc ~
+cp ~/Files/Conf/P53/90-backlight.conf /etc/udev/rules.d/
+cp ~/Files/Conf/P53/.ycm_extra_conf.py ~
+cp ~/Files/Conf/P53/.tmux.conf ~
+cp ~/Files/Conf/P53/compton.conf ~/.config
+cp -r ~/Files/Configs/P53/Scri ~/Files/Scri
--- P53/copy.sh
+++ P53/copy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cp ~/.vimrc ~/.xinitrc ~/.tmux.conf ~/Files/Configs/P53
-cp ~/.vim/syntax/clojure.vim ~/Files/Configs/P53
-cp ~/.config/picom.conf ~/Files/Configs/P53
-cp ~/.config/i3/config ~/Files/Configs/P53/i3/
-cp ~/.config/i3status/config ~/Files/Configs/P53/i3status/
-cp -r ~/Files/Scripts ~/Files/Configs/P53/
-cp /etc/udev/rules.d/90-backlight.rules ~/Files/Configs/P53
+cp ~/.vimrc ~/.xinitrc ~/.tmux.conf ~/Files/Conf/P53
+cp ~/.vim/syntax/clojure.vim ~/Files/Conf/P53
+cp ~/.config/picom.conf ~/Files/Conf/P53
+cp ~/.config/i3/config ~/Files/Conf/P53/i3/
+cp ~/.config/i3status/config ~/Files/Conf/P53/i3status/
+cp -r ~/Files/Scri ~/Files/Conf/P53/
+cp /etc/udev/rules.d/90-backlight.rules ~/Files/Conf/P53
 pacman -Qqe > pacman.txt
