129191264651739ae71837dca08bd2c7f56a469f
De Narm <s3152806@msx.tu-dresden.de>
2024-04-21 02:18:45 +0200

    [T460] removed some xmobar; added back win titles

--- T460/xmonad/xmonad.hs
+++ T460/xmonad/xmonad.hs
@@ -37,13 +37,23 @@ baseBGColor = "#1D1F21" ::[Char]   -- Header BG, normal windows
 baseHColor  = "#FFFFFF" ::[Char]   -- Highlight, Focus
 baseWColor  = "#F1C40F" ::[Char]   -- Warning, Prompt
 
-baseWhite   = "#FFFFFF" ::[Char]   -- xmobar workspaces text
-baseRed     = "#CC4444" ::[Char]   -- xmobar workspaces current
-baseLRed    = "#FF7777" ::[Char]   -- xmobar workspaces current
-baseBlue    = "#4444CC" ::[Char]   -- xmobar workspaces used
-baseLBlue   = "#7777FF" ::[Char]   -- xmobar workspaces used
-baseGrey    = "#444444" ::[Char]   -- xmobar workspaces empty
-baseLGrey   = "#777777" ::[Char]   -- xmobar workspaces empty
+baseWhite   = "#FFFFFF" ::[Char]   -- xmobar text
+
+baseLPurple = "#A569BD" ::[Char]   -- xmobar current title
+basePurple  = "#6C3483" ::[Char]   -- xmobar current title
+baseDPurple = "#5B2C6F" ::[Char]   -- xmobar current title
+
+baseLRed    = "#EC7063" ::[Char]   -- xmobar workspaces current
+baseRed     = "#B03A2E" ::[Char]   -- xmobar workspaces current
+baseDRed    = "#943126" ::[Char]   -- xmobar workspaces current
+
+baseLBlue   = "#5499C7" ::[Char]   -- xmobar workspaces used
+baseBlue    = "#1F618D" ::[Char]   -- xmobar workspaces used
+baseDBlue   = "#1A5276" ::[Char]   -- xmobar workspaces used
+
+baseLGrey   = "#99A3A4" ::[Char]   -- xmobar workspaces empty
+baseGrey    = "#616A6B" ::[Char]   -- xmobar workspaces empty
+baseDGrey   = "#515A5A" ::[Char]   -- xmobar workspaces empty
 
 --Main-------------------------------------------------------------------------
 
@@ -127,28 +137,45 @@ layouts = mkToggle (NBFULL ?? EOT)
 
 xmobarConfigs :: PP -- PP only, also see .config/xmobar/config
 xmobarConfigs = def
-    { ppSep             = xmobarColor baseWhite "" " • "
+    { ppSep             = xmobarColor baseGrey "" " • "
     , ppTitleSanitize   = xmobarStrip
-    , ppCurrent         = xmobarColor  baseWhite baseRed 
-                        . xmobarBorder "Bottom"  baseLRed   2
-                        . xmobarBorder "Top"     baseLRed   1
-                        . xmobarBorder "HBoth"   baseLRed   1
+    , ppCurrent         = xmobarColor  baseWhite baseDPurple 
+                        . xmobarBorder "Bottom"  baseLPurple 2
+                        . xmobarBorder "Top"     baseLPurple 1
+                        . xmobarBorder "HBoth"   baseLPurple 1
                         . wrap " " " " 
-    , ppHidden          = xmobarColor  baseWhite baseBlue 
-                        . xmobarBorder "Bottom"  baseLBlue  2
-                        . xmobarBorder "Top"     baseLBlue  1
-                        . xmobarBorder "HBoth"   baseLBlue  1
+    , ppHidden          = xmobarColor  baseWhite baseDBlue 
+                        . xmobarBorder "Bottom"  baseLBlue   2
+                        . xmobarBorder "Top"     baseLBlue   1
+                        . xmobarBorder "HBoth"   baseLBlue   1
                         . wrap " " " "
-    , ppHiddenNoWindows = xmobarColor  baseWhite baseGrey 
-                        . xmobarBorder "Bottom"  baseLGrey  2
-                        . xmobarBorder "Top"     baseLGrey  1
-                        . xmobarBorder "HBoth"   baseLGrey  1
+    , ppHiddenNoWindows = xmobarColor  baseWhite baseDGrey 
+                        . xmobarBorder "Bottom"  baseLGrey   2
+                        . xmobarBorder "Top"     baseLGrey   1
+                        . xmobarBorder "HBoth"   baseLGrey   1
                         . wrap " " " "
-    , ppUrgent          = xmobarColor  baseWhite baseRed 
-                        . xmobarBorder "Bottom"  baseWColor 2
-                        . xmobarBorder "Top"     baseWColor 1
-                        . xmobarBorder "HBoth"   baseWColor 1
+    , ppUrgent          = xmobarColor  baseWhite baseDRed 
+                        . xmobarBorder "Bottom"  baseLRed    2
+                        . xmobarBorder "Top"     baseLRed    1
+                        . xmobarBorder "HBoth"   baseLRed    1
                         . wrap " " " "
-    , ppOrder           = \[ws, layout, _master, _wins] -> [" " ++ ws, layout]
-    , ppExtras          = [logTitles xmobarRaw xmobarRaw]
+    , ppOrder           = \[ws, layout, _master, wins] 
+                        -> [" " ++ ws, layout, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
     }
+  where
+    formatFocused   = wrap (white "[") (white "]") . purple . ppWindow
+    formatUnfocused = wrap (grey  "[") (grey  "]") . blue . ppWindow
+
+    -- | Windows should have *some* title, which should not not exceed a
+    -- sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw 
+             . (\w -> if null w then "untitled" else w) 
+             . shorten 20
+
+    blue, grey, purple, white :: String -> String
+    white    = xmobarColor baseWhite   ""
+    blue     = xmobarColor baseLBlue   ""
+    purple   = xmobarColor baseLPurple ""
+    grey     = xmobarColor baseLGrey   ""
